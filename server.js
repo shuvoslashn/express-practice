17cb41509d595f4290a3bbb8047e0398869a334b
Shuvo Sarker <sarker3664@gmail.com>
2023-06-10 19:41:40 +0600

    test - check connection

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const port = 6000;
 // middlewares
 app.use(bodyParser.json());
 
+//? API to check connection
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to our app' });
+});
+
 // Server listening
 app.listen(port, () => {
     console.log(`Server running on ${port}`);
